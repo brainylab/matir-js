b849a0916f9c5082807024f2e6ff5aa577a55d77
André Felipe Schulle <andrefelipeschulle@icloud.com>
2026-06-02 17:33:11 -0300

    ajustes ao exportar os hooks que nãoe stavam sendo exportados corretamente

---
+++ .changeset/better-shoes-pay.md
@@ -0,0 +1,5 @@
+---
+"@matir-js/react": patch
+---
+
+ajustes ao exportar os hooks que nãoe stavam sendo exportados corretamente
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,4 +3,5 @@ export { matir } from "@matir-js/core";
 export * from "./can";
 export * from "./helpers/defineCanNav";
 export * from "./helpers/defineNav";
+export * from "./hooks/useCanNav";
 export * from "./matir-context";
